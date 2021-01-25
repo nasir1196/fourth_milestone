37f820228131b0f4bbd4f410b9579fa7d08a558f
Nasir Uddin <monforingnasir@gmail.com>
2021-01-25 15:11:38 +0600

    arguments

--- module-18/arguments.js
+++ module-18/arguments.js
@@ -1,5 +1,5 @@
-function addNumbers(num1,num2) {
-    //when using arguments inside function no need any parameters in function;
+function addNumbers(num1, num2) {
+  //when using arguments inside function no need any parameters in function;
   var sum = 0;
   for (let i = 0; i < arguments.length; i++) {
     const num = arguments[i];
@@ -7,5 +7,5 @@ function addNumbers(num1,num2) {
   }
   return sum;
 }
-var result=addNumbers(2,3,4,5,6,7,8);
+var result = addNumbers(2, 3, 4, 5, 6, 7, 8);
 console.log(result);
