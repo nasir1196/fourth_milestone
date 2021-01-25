c44a0d67559ad6d2fee388f974ae67fce84338cc
Nasir Uddin <monforingnasir@gmail.com>
2021-01-25 13:17:07 +0600

    solve quize

--- module-18/evenify.js
+++ module-18/evenify.js
@@ -10,3 +10,5 @@ function evenify(num) {
 }
 var result = [34, 54, 34, 2, 4, 5, 13, 7];
 evenify(result);
+
+
---
+++ module-18/quize-1.js
@@ -0,0 +1,5 @@
+if (25%5==0) {
+    console.log('Yes');    
+} else {
+    console.log("NO");
+}
