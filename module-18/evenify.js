22332c92ec6033931c5a66fee91558e38bd5d51b
Nasir Uddin <monforingnasir@gmail.com>
2021-01-25 13:02:37 +0600

    evenify function

---
+++ module-18/evenify.js
@@ -0,0 +1,12 @@
+function evenify(num) {
+  for (let i = 0; i < num.length; i++) {
+    const nums = num[i];
+    if (nums % 2 == 0) {
+      console.log(nums);
+    } else {
+      console.log(nums * 2);
+    }
+  }
+}
+var result = [34, 54, 34, 2, 4, 5, 13, 7];
+evenify(result);
