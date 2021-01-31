d6f17632681f13d6ab0e40f11d9b4472259130b3
Nasir Uddin <monforingnasir@gmail.com>
2021-01-31 13:49:32 +0600

    IIFE Immediately Invoking Function Expression

---
+++ module-21.5/IIFE.js
@@ -0,0 +1,11 @@
+// Immediately Invoked function express
+(function () {
+    let name = 'My Name';
+    console.log(name);
+}());
+
+var myFunction=function(){
+    var name="Antu";
+    console.log(name);
+}
+myFunction();
